71606c458b4fe017721e49d6e5e839ee4f2de9cb
Arina <ilamanova.arina@gmail.com>
2025-01-21 18:00:05 +0300

    Зарегистрировала в админке модели Category и Product

--- catalog/models.py
+++ catalog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    name_product = models.CharField(max_length=150, verbose_name='Название')
+    name = models.CharField(max_length=150, verbose_name='Название')
     description = models.CharField(max_length=150, verbose_name='Описание')
 
     def __str__(self):
-        return self.name_product
+        return self.name
 
     class Meta:
         verbose_name = 'Категория'
@@ -15,16 +15,16 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name_product = models.CharField(max_length=150, verbose_name='Название')
+    name = models.CharField(max_length=150, verbose_name='Название')
     description = models.CharField(max_length=150, verbose_name='Описание')
     imagery = models.ImageField(upload_to='photos/', verbose_name='Изображение', null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', null=True)
-    purchase_price = models.IntegerField(null=True)
+    price = models.IntegerField(null=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.name_product}  from the category: {self.category}.'
+        return f'{self.name}  from the category: {self.category}.'
 
     class Meta:
         verbose_name = 'Продукт'
