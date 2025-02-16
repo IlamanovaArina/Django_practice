2a4bbb3a5c139756f21d535213e92265810e62f8
Arina <ilamanova.arina@gmail.com>
2025-02-16 18:30:17 +0300

    Исправила наследование классов представления

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView, DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from catalog.models import Product
 from .forms import EditingForm
@@ -20,21 +21,21 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = EditingForm
     success_url = reverse_lazy('catalog:home')
     template_name = 'product_new.html'
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = EditingForm
     success_url = reverse_lazy('catalog:home')
     template_name = 'product_edit.html'
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:home')
     template_name = 'product_delete.html'
